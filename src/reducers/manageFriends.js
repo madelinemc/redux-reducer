16375de1296c87216eb1e299cfbfcc1a79651e8f
madelinemc <madeline.coons@gmail.com>
2021-02-15 17:53:52 -0600

    commit tree 019296cf2ad8b21cc96fa467a5f41f20861d5f93

--- src/reducers/manageFriends.js
+++ src/reducers/manageFriends.js
@@ -1,2 +1,24 @@
 export function manageFriends(state, action){
+    switch (action.type) {
+        case 'ADD_FRIEND':
+            return {...state, friends: [
+                ...state.friends, 
+                action.friend
+                ] 
+            }
+        case "REMOVE_FRIEND":
+            let removedFriendIndex = state.friends.findIndex(friend => friend.id === action.id);
+            return {...state, 
+                friends: [
+                    ...state.friends.slice(0, removedFriendIndex),
+                    ...state.friends.slice(removedFriendIndex + 1)
+                ]
+            }
+        default: 
+            return state;
+    }
 }
+
+let state = {
+    friends: []
+} 
